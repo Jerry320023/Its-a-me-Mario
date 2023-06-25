36e07cd8dcba62a6d996ed6208e27f4f647f3508
Jerry320023 <jerry320023@gmail.com>
2023-06-25 19:28:11 +0200

    Final

--- Zadanie 9/Program.cs
+++ Zadanie 9/Program.cs
@@ -23,6 +23,18 @@ namespace Mario
         static int buf;
         static int source;
 
+        //pozycja sluchacza X
+        static float listenerPosX = 0.0f;
+
+        //pozycja sluchacza Y
+        static float listenerPosY = 0.0f;
+
+        //pozycja sluchacza Z
+        static float listenerPosZ = 0.0f;
+
+        //glosnosc
+        static float volume = 0.01f;
+
         public static void KeyProcessor(IntPtr window, Keys key, int scanCode, InputState state, ModifierKeys mods)
         {
 
@@ -31,19 +43,58 @@ namespace Mario
 
             if (state == InputState.Press)
             {
-                if (key == Keys.Space)
+                if (key == Keys.Space) //obsluga klawisza spacja
                 {
                     if ((ALSourceState)sourcestate == ALSourceState.Paused)
                     {
-                        Console.WriteLine("Play");
+                        Console.WriteLine("Gra"); //uruchamia utwor
                         AL.SourcePlay(source);
                     }
                     if ((ALSourceState)sourcestate != ALSourceState.Paused)
                     {
-                        Console.WriteLine("Pause");
+                        Console.WriteLine("Zatrzymano"); //pauzuje utwor
                         AL.SourcePause(source);
                     }
                 }
+
+                if (key == Keys.Up)
+                {
+                    listenerPosZ -= 1f; //przenosi sluchacza do przodu o 1f 
+                }
+                else if (key == Keys.Down)
+                {
+                    listenerPosZ += 1f; //przenosi sluchacza do tylu o 1f 
+                }
+                else if (key == Keys.Left)
+                {
+                    listenerPosX -= 1f; //przenosi sluchacza w lewo o 1f 
+                }
+                else if (key == Keys.Right)
+                {
+                    listenerPosX += 1f; //przenosi sluchacza w prawo o 1f 
+                }
+
+                if (key == Keys.NumpadAdd)
+                {
+                    if (volume >= 0.1f) {
+                        Console.WriteLine("Nie radzę");
+                    }
+                    else {
+                    volume += 0.01f; //glosnosc wyzej o 0.01f
+                    }
+                }
+                if (key == Keys.NumpadSubtract)
+                {
+                    if (volume == 0)
+                    {
+                        Console.WriteLine("Dźwięki wyciszono");
+                    }
+                    else
+                    {
+                        volume -= 0.01f; //glosnosc wyzej o 0.01f
+                    }
+                }
+
             }
 
         }
@@ -53,8 +104,9 @@ namespace Mario
             device = ALC.OpenDevice(null);
             context = ALC.CreateContext(device, new ALContextAttributes());
             ALC.MakeContextCurrent(context);
-
             buf = AL.GenBuffer();
+
+            //Zdefiniowane nuty
             float C4 = 261.63f;
             float E4 = 329.63f;
             float Fs4 = 369.99f;
@@ -73,6 +125,8 @@ namespace Mario
             float A5 = 880.00f;
             float C6 = 1046.50f;
             
+
+            // Melodia
             float[] melody = {
 
             E5, E5, 0, E5, 0,
@@ -134,14 +188,20 @@ namespace Mario
             A4, B4, A4,0,
             Gs4,As4, Gs4,0,
             G4, Fs4, G4,0,0,0,
+            
+            };
 
- };
+            // Częstotliwość próbkowania 48 KHz
+            int sampleRate = 48000;
 
-            int sampleRate = 48000; // Częstotliwość próbkowania 48 KHz
-            double duration = 0.45; // Czas trwania pojedynczej nuty (w sekundach)
-            int totalSamples = (int)(sampleRate * duration); // Liczba próbek dla jednej nuty
+            // Czas trwania pojedynczej nuty (w sekundach)
+            double duration = 0.45;
 
-            short[] data = new short[melody.Length * totalSamples]; // Alokuj tablicę na wszystkie próbki
+            // Liczba próbek dla jednej nuty
+            int totalSamples = (int)(sampleRate * duration);
+
+            // Alokuj tablicę na wszystkie próbki
+            short[] data = new short[melody.Length * totalSamples]; 
 
             // Generuj próbki dla każdej nuty w sekwencji
             for (int i = 0; i < melody.Length; i++)
@@ -151,33 +211,54 @@ namespace Mario
                 {
                     
                     for (int j = 0; j < totalSamples; j++)
-                    {
+                    {   
+
+                        // Oblicz aktualny czas próbki
                         double time = (double)j / sampleRate;
+
+                        // Wygeneruj próbkę dźwiękową dla danej nuty w danym czasie
                         data[i * totalSamples + j] = signal(time, frequency, short.MaxValue);
                         
                     }
                 }
             }
 
+            // Przekazanie danych próbek do bufora dźwiękowego
             AL.BufferData(buf, ALFormat.Stereo16, data, sampleRate);
+
+            // Utworzenie źródła dźwięku
             source = AL.GenSource();
+
+            // Powiązanie bufora z źródłem dźwięku
             AL.BindBufferToSource(source, buf);
+
+            // Uruchomienie odtwarzania dźwięku
             AL.SourcePlay(source);
+
+            // Ustawienie zapętlenia odtwarzania
             AL.Source(source, ALSourceb.Looping, true);
         }
 
         public static void FreeSound()
         {
+            // Usunięcie źródła dźwięku
             AL.DeleteSource(source);
+
+            // Usunięcie bufora dźwiękowego
             AL.DeleteBuffer(buf);
 
+            // Zerowanie kontekstu OpenAL
             if (context != ALContext.Null)
             {
+                // Przełączenie na nullowy kontekst
                 ALC.MakeContextCurrent(ALContext.Null);
+
+                // Zniszczenie kontekstu
                 ALC.DestroyContext(context);
             }
             context = ALContext.Null;
 
+            // Zamknięcie urządzenia OpenAL
             if (device != ALDevice.Null)
             {
                 ALC.CloseDevice(device);
@@ -187,6 +268,7 @@ namespace Mario
 
         public static short signal(double t, double f, double A)
         {
+            // Generowanie próbki dźwiękowej dla danej nuty w danym czasie
             return (short)(A * Math.Sign(Math.Sin(0.85 * Math.PI * f * t)));
         }
 
@@ -194,38 +276,69 @@ namespace Mario
         {
 
             Glfw.Init();
-            Window window = Glfw.CreateWindow(500, 500, "Mario", GLFW.Monitor.None, Window.None);
+
+            // Tworzenie okna GLFW
+            Window window = Glfw.CreateWindow(200, 200, "Mario", GLFW.Monitor.None, Window.None);
+
+            // Ustawienie utworzonego okna jako bieżącego kontekstu
             Glfw.MakeContextCurrent(window);
+
+            // Ustawienie callbacka dla obsługi klawiatury
             Glfw.SetKeyCallback(window, kc);
+
+            // Inicjalizacja dźwięku
             InitSound();
             Console.Write(
 @"
 
-____▒▒▒▒▒
-—-▒▒▒▒▒▒▒▒▒
-—–▓▓▓░░▓░
-—▓░▓░░░▓░░░
-—▓░▓▓░░░▓░░░
-—▓▓░░░░▓▓▓▓ 
-——░░░░░░░░
-—-▓▓▒▓▓▓▒▓▓
-–▓▓▓▒▓▓▓▒▓▓▓
-▓▓▓▓▒▒▒▒▒▓▓▓▓
-░░▓▒░▒▒▒░▒▓░░
-░░░▒▒▒▒▒▒▒░░░
-░░▒▒▒▒▒▒▒▒▒░░
-—-▒▒▒ ——▒▒▒
-–▓▓▓———-▓▓▓
-▓▓▓▓———-▓▓▓▓
+
+░██████╗██╗░░░██╗██████╗░███████╗██████╗░  ███╗░░░███╗░█████╗░██████╗░██╗░█████╗░  ██████╗░██████╗░░█████╗░░██████╗
+██╔════╝██║░░░██║██╔══██╗██╔════╝██╔══██╗  ████╗░████║██╔══██╗██╔══██╗██║██╔══██╗  ██╔══██╗██╔══██╗██╔══██╗██╔════╝
+╚█████╗░██║░░░██║██████╔╝█████╗░░██████╔╝  ██╔████╔██║███████║██████╔╝██║██║░░██║  ██████╦╝██████╔╝██║░░██║╚█████╗░
+░╚═══██╗██║░░░██║██╔═══╝░██╔══╝░░██╔══██╗  ██║╚██╔╝██║██╔══██║██╔══██╗██║██║░░██║  ██╔══██╗██╔══██╗██║░░██║░╚═══██╗
+██████╔╝╚██████╔╝██║░░░░░███████╗██║░░██║  ██║░╚═╝░██║██║░░██║██║░░██║██║╚█████╔╝  ██████╦╝██║░░██║╚█████╔╝██████╔╝
+╚═════╝░░╚═════╝░╚═╝░░░░░╚══════╝╚═╝░░╚═╝  ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░╚════╝░  ╚═════╝░╚═╝░░╚═╝░╚════╝░╚═════╝░
+
+                                           ▒▒▒▒▒▒▒▒▒▄▄▄▄▒▒▒▒▒▒▒
+                                           ▒▒▒▒▒▒▄▀▀▓▓▓▀█▒▒▒▒▒▒
+                                           ▒▒▒▒▄▀▓▓▄██████▄▒▒▒▒
+                                           ▒▒▒▄█▄█▀░░▄░▄░█▀▒▒▒▒
+                                           ▒▒▄▀░██▄░░▀░▀░▀▄▒▒▒▒
+                                           ▒▒▀▄░░▀░▄█▄▄░░▄█▄▒▒▒
+                                           ▒▒▒▒▀█▄▄░░▀▀▀█▀▒▒▒▒▒
+                                           ▒▒▒▄▀▓▓▓▀██▀▀█▄▀▀▄▒▒
+                                           ▒▒█▓▓▄▀▀▀▄█▄▓▓▀█░█▒▒
+                                           ▒▒▀▄█░░░░░█▀▀▄▄▀█▒▒▒
+                                           ▒▒▒▄▀▀▄▄▄██▄▄█▀▓▓█▒▒
+                                           ▒▒█▀▓█████████▓▓▓█▒▒
+                                           ▒▒█▓▓██▀▀▀▒▒▒▀▄▄█▀▒▒
+                                           ▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
 
 ");
 
+
+            Console.WriteLine("Klawiszami + i - na numpadzie można zmieniać głośność odtwarzania.");
+            Console.WriteLine("Klawiszem Spacja można zatrzymywać i odtwarzać utwór.");
+            Console.WriteLine("Strzałkami można zmieniać pozycję 'słuchacza'.");
+
+
             while (!Glfw.WindowShouldClose(window))
             {
+                // Pobieranie i obsługa zdarzeń okna
                 Glfw.PollEvents();
+
+                // Aktualizacja pozycji słuchacza dźwięku
+                AL.Listener(ALListener3f.Position, listenerPosX, listenerPosY, listenerPosZ);
+
+                // Ustawienie poziomu głośności słuchacza
+                AL.Listener(ALListenerf.Gain, volume);
             }
 
+            // Zwolnienie zasobów dźwiękowych
             FreeSound();
+
+            // Zakończenie biblioteki GLFW
             Glfw.Terminate();
         }
     }
